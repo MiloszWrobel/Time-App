0800f9abf365ddbe61c542a5b6306628009f3ddb
MiloszWrobel <100210720+MiloszWrobel@users.noreply.github.com>
2024-01-04 13:07:25 +0000

    Updated the set name button functionality this time using the useRef state

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,22 +1,20 @@
-import { useState } from "react";
+import { useState, useRef } from "react";
 
 export default function Player() {
-  const [playerName, setPlayerName] = useState(null);
-  const [submitted, setSubmitted] = useState(false);
+  const playerName = useRef();
+  const [enteredPlayerName, setEnteredPlayerName] = useState(null);
 
   function handleClick() {
-    setSubmitted(true);
-  }
-  function handleChange(event) {
-    setSubmitted(false);
-    setPlayerName(event.target.value);
+    setEnteredPlayerName(playerName.current.value);
   }
 
   return (
     <section id="player">
-      <h2>Welcome {submitted ? playerName : " unkown entity"}</h2>
+      <h2>
+        Welcome {enteredPlayerName ? enteredPlayerName : " unkown entity"}
+      </h2>
       <p>
-        <input type="text" onChange={handleChange} value={playerName} />
+        <input ref={playerName} type="text" />
         <button onClick={handleClick}>Set Name</button>
       </p>
     </section>
