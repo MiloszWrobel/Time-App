5713f2309b57c290a540f84fd217daa9b4682eaa
MiloszWrobel <100210720+MiloszWrobel@users.noreply.github.com>
2024-01-04 12:57:39 +0000

    Added the set name functionality using useState principles

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,10 +1,23 @@
+import { useState } from "react";
+
 export default function Player() {
+  const [playerName, setPlayerName] = useState(null);
+  const [submitted, setSubmitted] = useState(false);
+
+  function handleClick() {
+    setSubmitted(true);
+  }
+  function handleChange(event) {
+    setSubmitted(false);
+    setPlayerName(event.target.value);
+  }
+
   return (
     <section id="player">
-      <h2>Welcome unknown entity</h2>
+      <h2>Welcome {submitted ? playerName : " unkown entity"}</h2>
       <p>
-        <input type="text" />
-        <button>Set Name</button>
+        <input type="text" onChange={handleChange} value={playerName} />
+        <button onClick={handleClick}>Set Name</button>
       </p>
     </section>
   );
