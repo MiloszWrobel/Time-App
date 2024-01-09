2f19607345f1e53b61108b51a6e0c233f90ce2e0
MiloszWrobel <100210720+MiloszWrobel@users.noreply.github.com>
2024-01-09 14:42:59 +0000

    Created time count functionality for game plus updated components

--- src/components/ResultModal.jsx
+++ src/components/ResultModal.jsx
@@ -1,23 +1,29 @@
 import { forwardRef, useImperativeHandle, useRef } from "react";
 
 const ResultModal = forwardRef(function ResultModal(
-  { result, targetTime },
+  { targetTime, remainingTime },
   ref
 ) {
-  useRef;
+  const dialog = useRef();
+
+  const userLost = remainingTime <= 0;
+  const formattedRemainingTime = (remainingTime / 1000).toFixed(2);
   useImperativeHandle(ref, () => {
     return {
-      open() {},
+      open() {
+        dialog.current.showModal();
+      },
     };
   });
   return (
-    <dialog ref={ref} className="result-modal">
-      <h2>You {result}</h2>
+    <dialog ref={dialog} className="result-modal">
+      {userLost && <h2>You lost </h2>}
       <p>
         The target time was <strong>{targetTime} seconds.</strong>
       </p>
       <p>
-        You stopped the timer with <strong>X seconds left</strong>
+        You stopped the timer with{" "}
+        <strong>{formattedRemainingTime} seconds left.</strong>
       </p>
       <form method="dialog">
         <button>Close</button>
