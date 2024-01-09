e180162434ec72f61c3e7b3bc6acbd8ad13a8059
MiloszWrobel <100210720+MiloszWrobel@users.noreply.github.com>
2024-01-09 15:13:36 +0000

    Finished game functionality

--- src/components/TimerChallenge.jsx
+++ src/components/TimerChallenge.jsx
@@ -10,10 +10,11 @@ export default function TimerChallenge({ title, targetTime }) {
 
   if (timeRemaining <= 0) {
     clearInterval(timer.current);
-    setTimeRemaining(targetTime * 1000);
     dialog.current.open();
   }
-
+  function handleReset() {
+    setTimeRemaining(targetTime * 1000);
+  }
   function handleStart() {
     timer.current = setInterval(() => {
       setTimeRemaining((prevTimeRemaining) => prevTimeRemaining - 10);
@@ -29,6 +30,7 @@ export default function TimerChallenge({ title, targetTime }) {
         ref={dialog}
         targetTime={targetTime}
         remainingTime={timeRemaining}
+        onReset={handleReset}
       />
 
       <section className="challenge">
